d8ce304804d7b6eaa8d39f993d6db4a65571aa18
Jardan Andrei <jardan.andrei@gmail.com>
2023-10-28 08:46:46 +0300

    get rid of cors

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,12 @@
 import flask
+import flask_cors
 
 import db
 
 # setup stuff
 app = flask.Flask(__name__)
 engine = sqlalchemy.create_engine("sqlite:///data.db")
+flask_cors.CORS(app)  # get rid of cors
 
 
 # NOTE(andreij): sample flask route
