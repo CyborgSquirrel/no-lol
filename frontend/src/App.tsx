6e6bc6c3423197a2067c6d7c5e53b99b8a39c8fe
Ichim Stefan <stefanichim2201@gmail.com>
2023-12-08 12:34:46 +0200

    Add register page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,53 +1,76 @@
-import './App.css';
+import "./App.css";
 import axios from "axios";
 import React from "react";
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
-import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
-import {Box} from "@mui/material";
-import {Colors} from "./assets/Colors";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Box } from "@mui/material";
+import { Colors } from "./assets/Colors";
 import ProfilePage from "./pages/ProfilePage";
 import LoginPage from "./pages/LoginPage";
 import ErrorPage from "./pages/ErrorPage";
-import {BACKEND_API_URL} from "./constants";
+import { BACKEND_API_URL } from "./constants";
+import RegisterPage from "./pages/RegisterPage";
 
 const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <LoginPage/>,
-        errorElement: <ErrorPage/>
-    },
-    {
-        path: "/profile/:id",
-        element: (
-            <Box sx = {{
-                position: "absolute",
-                top: 0,
-                left: 0,
-                width: "100vw",
-                height: "100vh",
-                backgroundColor: Colors.RICH_BLACK,
-                padding: "0",
-                margin: "0"
-            }}>
-                <ProfilePage/>
-            </Box>
-        ),
-        errorElement: <ErrorPage/>
-    }
+  {
+    path: "/",
+    element: <LoginPage />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/profile/:id",
+    element: (
+      <Box
+        sx={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100vw",
+          height: "100vh",
+          backgroundColor: Colors.RICH_BLACK,
+          padding: "0",
+          margin: "0",
+        }}
+      >
+        <ProfilePage />
+      </Box>
+    ),
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/register",
+    element: (
+      <Box
+        sx={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100vw",
+          height: "100vh",
+          backgroundColor: Colors.RICH_BLACK,
+          padding: "0",
+          margin: "0",
+        }}
+      >
+        <RegisterPage />
+      </Box>
+    ),
+    errorElement: <ErrorPage />,
+  },
 ]);
 
 function App() {
-    // set the backend url
-    axios.defaults.baseURL = BACKEND_API_URL;
+  // set the backend url
+  axios.defaults.baseURL = BACKEND_API_URL;
 
-    // create a React Query client instance
-    const queryClient = new QueryClient();
+  // create a React Query client instance
+  const queryClient = new QueryClient();
 
-    return (
-        <QueryClientProvider client={queryClient}>
-            <RouterProvider router={router}/>
-        </QueryClientProvider>
-    );
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
