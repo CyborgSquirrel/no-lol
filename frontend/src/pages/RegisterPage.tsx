ee6adefef374c0541aa8c530305ef599f959ff21
Ichim Stefan <stefanichim2201@gmail.com>
2023-12-11 10:54:50 +0200

    Add region dropdown and retype password input field

--- frontend/src/pages/RegisterPage.tsx
+++ frontend/src/pages/RegisterPage.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import {
     Box,
     FormControl,
@@ -7,12 +8,13 @@ import {
     InputAdornment,
     IconButton,
     Button,
+    Select,
+    MenuItem,
 } from "@mui/material";
 import { Visibility, VisibilityOff } from "@mui/icons-material";
 import { Colors } from "../assets/Colors";
 import { Navigate, Link } from "react-router-dom";
 import axios from "axios";
-import React from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
@@ -27,8 +29,13 @@ function RegisterPage() {
 
     const [username, setUsername] = React.useState("");
     const [password, setPassword] = React.useState("");
+    const [retypePassword, setRetypePassword] = React.useState("");
     const [summonerName, setSummonerName] = React.useState("");
     const [region, setRegion] = React.useState("");
+
+    const handleRetypePasswordChange = (event: {
+        target: { value: React.SetStateAction<string> };
+    }) => setRetypePassword(event.target.value);
 
     const handleRegister = () => {
         const data = {
@@ -41,27 +48,26 @@ function RegisterPage() {
         if (
             username === "" ||
             password === "" ||
+            retypePassword === "" ||
             summonerName === "" ||
             region === ""
         ) {
-            toast.warning("Toate field-urile trebuie completate.");
+            toast.warning("All fields must be filled out.");
+        } else if (password !== retypePassword) {
+            toast.warning("Passwords do not match.");
         } else {
             axios
                 .post("/user/register", data)
                 .then((response) => {
-                    console.log(
-                        "Inregistrare realizata cu success:",
-                        response.data
-                    );
+                    console.log("Registration successful:", response.data);
 
-                    // Back to home page
+                    // Redirect to the home page
                     return <Navigate to={"/"} />;
                 })
                 .catch((error) => {
                     if (error.response) {
-                        // maybe not the best approach
                         if (error.response.data.name_already_exists) {
-                            toast.error("Name already exists!");
+                            toast.error("Username already exists!");
                         } else if (
                             error.response.data.summoner_already_exists
                         ) {
@@ -70,11 +76,30 @@ function RegisterPage() {
                             toast.error(error.response.data);
                         }
                     } else {
-                        toast.error("Nu se poate conecta la server.");
+                        toast.error("Unable to connect to the server.");
                     }
                 });
         }
     };
+
+    const regions = [
+        "NA",
+        "EUW",
+        "EUN",
+        "KR",
+        "BR",
+        "JP",
+        "RU",
+        "OCE",
+        "TR",
+        "LAN",
+        "LAS",
+        "PH",
+        "SG",
+        "TH",
+        "TW",
+        "VN",
+    ];
 
     return (
         <Box
@@ -142,6 +167,38 @@ function RegisterPage() {
 
                 <br />
 
+                <TextField
+                    sx={{
+                        width: "250px",
+                        "& label": {
+                            color: Colors.WHITE_BLUE,
+                        },
+                        "& .MuiInputBase-input": {
+                            color: Colors.WHITE_BLUE,
+                        },
+                        "& label.Mui-focused": {
+                            color: Colors.WHITE_BLUE,
+                        },
+                        "& .MuiInput-underline:before": {
+                            borderBottomColor: Colors.WHITE_BLUE,
+                        },
+                        "& .MuiInput-underline:after": {
+                            borderBottomColor: Colors.WHITE_BLUE,
+                        },
+                        "&:hover .MuiInput-underline:before": {
+                            borderBottomColor: Colors.WHITE_BLUE,
+                        },
+                    }}
+                    id="summonerName"
+                    label="Summoner Name"
+                    type="search"
+                    variant="standard"
+                    value={summonerName}
+                    onChange={(e) => setSummonerName(e.target.value)}
+                />
+
+                <br />
+
                 <FormControl
                     sx={{
                         m: 1,
@@ -193,8 +250,9 @@ function RegisterPage() {
 
                 <br />
 
-                <TextField
+                <FormControl
                     sx={{
+                        m: 1,
                         width: "250px",
                         "& label": {
                             color: Colors.WHITE_BLUE,
@@ -215,17 +273,22 @@ function RegisterPage() {
                             borderBottomColor: Colors.WHITE_BLUE,
                         },
                     }}
-                    id="summonerName"
-                    label="Summoner Name"
-                    type="search"
                     variant="standard"
-                    value={summonerName}
-                    onChange={(e) => setSummonerName(e.target.value)}
-                />
+                >
+                    <InputLabel htmlFor="retypePassword">
+                        Retype Password
+                    </InputLabel>
+                    <Input
+                        id="retypePassword"
+                        type="password"
+                        value={retypePassword}
+                        onChange={handleRetypePasswordChange}
+                    />
+                </FormControl>
 
                 <br />
 
-                <TextField
+                <FormControl
                     sx={{
                         width: "250px",
                         "& label": {
@@ -247,13 +310,20 @@ function RegisterPage() {
                             borderBottomColor: Colors.WHITE_BLUE,
                         },
                     }}
-                    id="region"
-                    label="Region"
-                    type="search"
-                    variant="standard"
-                    value={region}
-                    onChange={(e) => setRegion(e.target.value)}
-                />
+                >
+                    <InputLabel htmlFor="region">Region</InputLabel>
+                    <Select
+                        id="region"
+                        value={region}
+                        onChange={(e) => setRegion(e.target.value)}
+                    >
+                        {regions.map((regionOption) => (
+                            <MenuItem key={regionOption} value={regionOption}>
+                                {regionOption}
+                            </MenuItem>
+                        ))}
+                    </Select>
+                </FormControl>
 
                 <br />
 
